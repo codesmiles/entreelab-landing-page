92e11690f7d250e5074b53a2afec4767c7ca6823
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-10-11 23:20:09 +0100

    Update

--- components/landing-page/requestDemo.js
+++ components/landing-page/requestDemo.js
@@ -1,79 +1,46 @@
+import data from "./sub-components/formData";
+
 const RequestDemo = () => {
-  const data = [
-    {
-      name: `fname`,
-      text: `first name*`,
-      type: `text`,
-      placeholder: `enter your first name`,
-    },
-    {
-      name: `lname`,
-      text: `last name*`,
-      type: `text`,
-      placeholder: `enter your last name`,
-    },
-    {
-      name: `phone`,
-      text: `phone no.`,
-      type: `tel`,
-      placeholder: `enter phone no`,
-    },
-    {
-      name: `businessName`,
-      text: `business name`,
-      type: `text`,
-      placeholder: `what's your business name`,
-    },
-    {
-        name: `businessEmail`,
-        text: `business email`,
-        type: `text`,
-        placeholder: `what's your business email`,
-      },
-      {
-        name: `jobTitle`,
-        text: `job title`,
-        type: `text`,
-        placeholder: `what's your job title`,
-      }
-  ];
   return (
     <section className={`bg-[#3397C8] py-10 text-["Lato"]`}>
       <h1 className={`text-center text-[#07131A] font-bold text-[20px] mb-5`}>
         Request a Demo
       </h1>
-      <form action="/" method="post">
-        <div>
-          <label htmlFor="firstName">first name</label>
-          <input type="text" name="firstName" id="firstName" />
-        </div>
-        <div>
-          <label htmlFor="lastName">last name</label>
-          <input type="text" name="lastName" id="lastName" />
-        </div>
-        <div>
-          <label htmlFor="contact">phone no.</label>
-          <input type="phone" name="contact" id="contact" />
-        </div>
-        <div>
-          <label htmlFor="businessName">business name</label>
-          <input type="text" name="businessName" id="businessName" />
-        </div>
-        <div>
-          <label htmlFor="businessEmail">business email</label>
-          <input type="email" name="businessEmail" id="businessEmail" />
-        </div>
-        <div>
-          <label htmlFor="jobTitle">job title</label>
-          <input type="text" name="jobTitle" id="jobTitle" />
-        </div>
+      <form action="/" method="post" className="w-4/5 mx-auto">
+        {data.map((item, index) => {
+          return (
+            <div key={index} className={`mb-5`}>
+              <label
+                htmlFor={item.name}
+                className={`block capitalize text-[#07131A] font-semibold`}
+              >
+                {item.text}
+              </label>
+              <input
+                type={item.type}
+                name={item.name}
+                id={item.name}
+                placeholder={item.placeholder}
+                className={`w-full rounded-lg py-1 pl-1 placeholder:capitalize placeholder:text-[#B7B7B7] placeholder:text-[15px]`}
+                required
+              />
+            </div>
+          );
+        })}
 
-        <label htmlFor="additionalComment">additional comment</label>
+        <label
+          htmlFor="additionalComment"
+          className={`block text-[#07131A] font-semibold`}
+        >
+          Any additional comment
+        </label>
         <textarea
           name="additionalComment"
           id="additionalComment"
-          cols="30"
-          rows="10"
+          //   cols="30"
+          rows="4"
+          placeholder="Add any additional comments/questions here"
+          className={`w-full rounded-lg placeholder:text-[#B7B7B7] placeholder:text-[15px] pt-1 pl-3`}
         ></textarea>
         <button type="submit">submit</button>
       </form>
