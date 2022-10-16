70f1d1702870d51a3f9adf93c6c8326905f8472b
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-10-16 01:22:49 +0100

    update

--- components/landing-page/aboutEntreelab.js
+++ components/landing-page/aboutEntreelab.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { MainHeader } from "./sub-components/header";
+import aboutEntreelabDesktop from "../../public/landing-page/about-entreelab-png.png";
+import aboutEntreelabMobile from "../../public/landing-page/about-entreelab-mobile.png";
+
 const AboutEntreelab = () => {
   return (
     <section className="relative md:flex">
       <div>
         <div className="md:hidden absolute -top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5">
           <Image
-            src={`/landing-page/mobile-about-us.svg`}
+            src={aboutEntreelabMobile}
+            placeholder="blur"
             width={`335px`}
             height={`220px`}
             className="h-full"
@@ -14,7 +18,8 @@ const AboutEntreelab = () => {
         </div>
         <div className={`hidden md:block bg-[url("/landing-page/about-entreelab-jpg.jpg")] bg-cover bg-no-repeat`}>
           <Image
-            src={`/landing-page/about-entreelab-png.png`}
+            src={aboutEntreelabDesktop}
+            placeholder="blur"
             width={`574px`}
             height={`503px`}
           />
@@ -22,7 +27,7 @@ const AboutEntreelab = () => {
       </div>
       <div className={`text-center bg-[#0F2D3C] text-white pt-20 px-4 pb-16 md:w-2/3 md:py-0 md:text-left lg:py-10 lg:px-24 xl:py-28 xl:px-32`}>
         <div className=" mb-5">
-          <MainHeader text="About EntréeLab" />
+          <MainHeader text="About EntréeLab"/>
         </div>
         <p className="leading-8 lg:w-11/12">
           We are a software development company in Nigeria that works with
